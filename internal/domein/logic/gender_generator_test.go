5a04d85f986c3216cfcb8013a514c6027dd314c5
Yukihiro Arisawa <42995313+iamyukihiro@users.noreply.github.com>
2024-06-16 23:52:40 +0900

    Update gender_generator_test.go

--- internal/domein/logic/gender_generator_test.go
+++ internal/domein/logic/gender_generator_test.go
@@ -17,7 +17,7 @@ func TestGenerate(t *testing.T) {
 		assert.Equal(t, enum.Male, actual)
 	})
 
-	t.Run("ジェンダーバイアスが100だった場合、Female が返されること", func(t *testing.T) {
+	t.Run("ジェンダーバイアスが0だった場合、Female が返されること", func(t *testing.T) {
 		SUT := &GenderGenerator{
 			probability: NewProbability(),
 			genderBias:  0,
